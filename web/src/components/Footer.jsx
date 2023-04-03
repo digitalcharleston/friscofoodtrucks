e5b7c5b04f66d7a21c30cae83c75e61c39448065
David Cole <davidcole@users.noreply.github.com>
2023-04-03 17:01:37 -0400

    Cleanup

--- web/src/components/Footer.jsx
+++ web/src/components/Footer.jsx
@@ -1,6 +1,6 @@
 const Footer = () => (
   <footer className="pt-5 my-5 text-muted border-top">
-    Created with ❤️ for the folks at Estee &copy; {new Date().getFullYear()}
+    &copy; {new Date().getFullYear()} Digital Charleston LLC
   </footer>
 );
 
